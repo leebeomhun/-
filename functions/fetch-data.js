432c5ab1019554897c3b5d964964c43d7ba0a8d0
leebeomhun <77260896+leebeomhun@users.noreply.github.com>
2024-02-25 13:49:16 +0900

    Add files via upload

--- functions/fetch-data.js
+++ functions/fetch-data.js
@@ -1,11 +1,8 @@
 // functions/fetch-data.js
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 export async function handler(event, context) {
-  // 환경변수에서 API 키 가져오기
   const apiKey = process.env.LOSTARK_API_KEY;
-
-  // 쿼리스트링에서 캐릭터 이름 가져오기
   const characterName = event.queryStringParameters.name;
   if (!characterName) {
     return {
@@ -14,30 +11,21 @@ export async function handler(event, context) {
     };
   }
 
-  const url = `https://developer-lostark.game.onstove.com/armories/characters/${encodeURIComponent(characterName)}`; // URL에 캐릭터 이름 포함
+  const url = `https://developer-lostark.game.onstove.com/armories/characters/${encodeURIComponent(characterName)}`;
 
   try {
-    const response = await fetch(url, {
+    const response = await axios.get(url, {
       headers: {
         'accept': 'application/json',
-        'Authorization': `Bearer ${apiKey}` // API 키 사용
+        'Authorization': `Bearer ${apiKey}`
       }
     });
 
-    if (!response.ok) {
-      // 요청 실패 처리
-      return { statusCode: response.status, body: response.statusText };
-    }
-
-    const data = await response.json();
-
-    // 성공적으로 데이터를 받아왔을 때
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(response.data)
     };
   } catch (error) {
-    // 에러 처리
     return { statusCode: 500, body: JSON.stringify({ msg: error.message }) };
   }
 };
